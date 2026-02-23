b2d962f87a854f0ce85fd1d49f8ada08e0e2fc5d
Matheus Trindade <matheust.rocha47@gmail.com>
2026-02-23 15:36:56 -0300

    questão 1 com menu

---
+++ list1.py
@@ -0,0 +1,35 @@
+
+def atv1():
+    def recursive_multi(a, b):
+        if b == 0:
+            return 0
+        return a + recursive_multi(a, b - 1)
+
+    a = (int(input("Enter the first number: ")))
+    b = (int(input("Enter the second number: ")))
+    print(recursive_multi(a,b))
+
+def atv2():
+    
+
+def atv3():
+
+def atv4():
+
+def atv5():
+
+def atv6():
+
+def atv7():
+
+def atv8():
+
+def main():
+    atv1()
+    atv2()
+    atv3()
+    atv4()
+    atv5()
+    atv6()
+    atv7()
+    atv8()
