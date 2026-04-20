8f7a433c3d299bd358513299805d6def2eeb5f0e
Matheus Trindade <matheust.rocha47@gmail.com>
2026-04-19 21:17:58 -0300

    add: parte 2

--- Lista_6/list6.cs
+++ Lista_6/list6.cs
@@ -1,6 +1,55 @@
 using System;
 
-//Parte 1
+//Parte 1===============================================================
+// class Aluno
+// {
+//     private string nome;
+
+//     public string GetNome()
+//     {
+//         return nome;
+//     }
+
+//     public void SetNome(string nome)
+//     {
+//         this.nome = nome;
+//     }
+
+//     public Aluno(string nome)
+//     {
+//         this.nome = nome;
+//     }
+
+//     public void ExibirNome()
+//     {
+//         Console.WriteLine("Aluno: " + nome);
+//     }
+// }
+
+// class EscolaDependencia
+// {
+//     public void ExibirAluno(Aluno aluno)
+//     {
+//         Console.WriteLine("Exibindo aluno (Dependência): " + aluno.GetNome());
+//     }
+// }
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         EscolaDependencia escola = new EscolaDependencia();
+//         Aluno aluno = new Aluno("Matheus");
+
+//         escola.ExibirAluno(aluno);
+
+//         Console.ReadLine();
+//     }
+// }
+
+
+//Parte 2 ===============================================================
+
 class Aluno
 {
     private string nome;
@@ -26,11 +75,18 @@ class Aluno
     }
 }
 
-class EscolaDependencia
+class EscolaAssociacao
 {
-    public void ExibirAluno(Aluno aluno)
+    private Aluno aluno;
+
+    public EscolaAssociacao(Aluno aluno)
     {
-        Console.WriteLine("Exibindo aluno (Dependência): " + aluno.GetNome());
+        this.aluno = aluno;
+    }
+
+    public void ExibirAluno()
+    {
+        Console.WriteLine("Exibindo aluno (Associação): " + aluno.GetNome());
     }
 }
 
@@ -38,10 +94,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        EscolaDependencia escola = new EscolaDependencia();
         Aluno aluno = new Aluno("Matheus");
+        EscolaAssociacao escola = new EscolaAssociacao(aluno);
 
-        escola.ExibirAluno(aluno);
+        escola.ExibirAluno();
 
         Console.ReadLine();
     }
